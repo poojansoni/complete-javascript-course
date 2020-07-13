dd58fba937d0e20547180f18203991ef75c8a183
Poojan Soni <poojansoni.udr@gmail.com>
2020-07-13 19:45:38 +0530

    Data types in JS

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,1 +1,7 @@
 console.log("Hello World!!!");
+var firstName = "Poojan";
+var lastName = "Soni"; //string
+var age = 1000; //seems int but is floating point decimal
+var trueAge = false; //boolean
+var xyz; //undefined
+var check = null;
