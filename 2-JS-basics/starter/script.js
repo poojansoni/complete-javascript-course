74669ea82a1faf9ea13249334c09e0e5b9b5b02a
Poojan Soni <poojansoni.udr@gmail.com>
2020-07-14 16:52:37 +0530

    Variable mutation, coersion, alerts, prompts

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -1,7 +1,18 @@
 console.log("Hello World!!!");
+
+
 var firstName = "Poojan";
 var lastName = "Soni"; //string
 var age = 1000; //seems int but is floating point decimal
 var trueAge = false; //boolean
 var xyz; //undefined
 var check = null;
+
+
+//type coersion
+console.log(firstName + " "+ lastName + " is "+ age + " years old which is obviously "+ trueAge);
+
+alert(firstName + " "+ lastName + " is "+ age + " years old which is obviously "+ trueAge);
+
+var yourName = prompt("What is your name?");
+console.log("Your name is "+ yourName);
