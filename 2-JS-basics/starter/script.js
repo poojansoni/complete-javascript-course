9b2b8c3a6d5bcfbf7cb10eb2784e0b185638f99b
Poojan Soni <poojansoni.udr@gmail.com>
2020-07-16 23:03:43 +0530

    Coding Challenge 5

--- 2-JS-basics/starter/script.js
+++ 2-JS-basics/starter/script.js
@@ -127,33 +127,138 @@ Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and heig
 
 GOOD LUCK 😀
 */
-var Mark = {
-    fullname: "Mark Smith",
-    mass: 78,
-    height: 1.69,
-    calculateBMI: function(){
-        this.BMI = this.mass/(this.height * this.height);
-        return this.BMI;
-    }
-};
-var John = {
-    fullname: "John Simpson",
-    mass: 92,
-    height: 1.95,
-    calculateBMI: function(){
-        this.BMI = this.mass/(this.height*this.height);
-        return this.BMI;
-    }
-};
-// console.log(John.BMI);
+// var Mark = {
+//     fullname: "Mark Smith",
+//     mass: 78,
+//     height: 1.69,
+//     calculateBMI: function(){
+//         this.BMI = this.mass/(this.height * this.height);
+//         return this.BMI;
+//     }
+// };
+// var John = {
+//     fullname: "John Simpson",
+//     mass: 92,
+//     height: 1.95,
+//     calculateBMI: function(){
+//         this.BMI = this.mass/(this.height*this.height);
+//         return this.BMI;
+//     }
+// };
+// // console.log(John.BMI);
 
 
-if(Mark.calculateBMI()>John.calculateBMI()){
-    console.log(Mark.fullname+" has higher BMI with value: "+ Mark.BMI);
-}
-else if(John.BMI === Mark.BMI){
-    console.log(Mark.fullname+" and "+ John.fullname +" have same BMI with value: "+ Mark.BMI);
-}
-else{
-    console.log(John.fullname+" has higher BMI with value: "+ John.BMI);
+// if(Mark.calculateBMI()>John.calculateBMI()){
+//     console.log(Mark.fullname+" has higher BMI with value: "+ Mark.BMI);
+// }
+// else if(John.BMI === Mark.BMI){
+//     console.log(Mark.fullname+" and "+ John.fullname +" have same BMI with value: "+ Mark.BMI);
+// }
+// else{
+//     console.log(John.fullname+" has higher BMI with value: "+ John.BMI);
+// }
+
+
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is 
+more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in 
+each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by 
+the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+    
+     var johnBill = {
+                  value: [124,48,268,180,42] ,
+                  tip: [],
+                  amount: [],
+                  tipCalculator: function(){
+                      for(var i = 0; i<this.value.length ; i++){
+                        if(this.value[i]<50){
+                            this.tip[i] = this.value[i]*0.2;
+                            this.amount[i] = this.tip[i] + this.value[i];
+                        }
+                        else if(this.value[i]>=100 && this.value[i]<300){
+                            this.tip[i] = this.value[i]*0.15;
+                            this.amount[i] = this.tip[i] + this.value[i];
+                        }
+                        else{
+                            this.tip[i] = this.value[i]*0.1;
+                            this.amount[i] = this.tip[i] + this.value[i];
+                        }
+                    }
+                }
+            };
+        johnBill.tipCalculator();
+        console.log(johnBill.tip,johnBill.amount);
+
+
+        var markBill = {
+            value: [77,375,110,45] ,
+            tip: [],
+            amount: [],
+            tipCalculator: function(){
+                for(var i = 0; i<this.value.length ; i++){
+                  if(this.value[i]<100){
+                      this.tip[i] = this.value[i]*0.2;
+                      this.amount[i] = this.tip[i] + this.value[i];
+                  }
+                  else if(this.value[i]>=50 && this.value[i]<200){
+                      this.tip[i] = this.value[i]*0.1;
+                      this.amount[i] = this.tip[i] + this.value[i];
+                  }
+                  else{
+                      this.tip[i] = this.value[i]*0.25;
+                      this.amount[i] = this.tip[i] + this.value[i];
+                  }
+              }
+          }
+      };
+      markBill.tipCalculator();
+      console.log(markBill.tip,markBill.amount);
+
+      var avgTip = function(tips){
+          var sum = 0;
+        for(var i = 0; i< tips.length; i++)
+            sum = sum + tips[i];
+      return sum/tips.length;
+      }
+
+    
+    johnBill.averageTip = avgTip(johnBill.tip);
+    markBill.averageTip = avgTip(markBill.tip);
+
+
+      console.log("Averge tip for John "+ avgTip(johnBill.tip)+" , average tip for Mark is "+ avgTip(markBill.tip));
+            
+if(johnBill.averageTip>markBill.averageTip){
+    console.log("Averge tip for John is highest : "+ avgTip(johnBill.tip));
+}else if( johnBill.averageTip<markBill.averageTip){
+    console.log("Averge tip for Mark is highest : "+ avgTip(markBill.tip));
+}else{
+    console.log("Averge tip for John and Mark is equal : "+ avgTip(johnBill.tip));
 }
